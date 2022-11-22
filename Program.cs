c7a2f32be4fbd9dcecc7774b33e306da16e2f7e8
OleganVS <118183808+OleganVS@users.noreply.github.com>
2022-11-22 20:41:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            for (int  i = 1;  i < n; i++) 
+            {
+                s = 0 + 2 * i - 1;
+                s = 2 + 2 * i - 1;
+                s = 3 + 2 * i - 1;
+                s = 4 + 2 * i - 1;
+                break; 
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
